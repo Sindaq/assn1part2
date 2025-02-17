da23b27595263d169e453b0cf438354b118556e0
Sindaq <gabesindac1998@gmail.com>
2025-02-16 21:39:06 -0600

    before

---
+++ src/test/java/org/example/NumberUtilsTest.java
@@ -0,0 +1,304 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class NumberUtilsTest {
+
+    NumberUtils util = new NumberUtils();
+    /**
+     *
+     * Step 1: understand the requirement, input type and output type
+     *        Requirement: Add two list of integer, index by index, and returns another list
+     *        Input type:
+     *          Left: Integer List
+     *          Right: Integer List
+     *        Output type: Integer List
+     * Step 2 (raw):  Perform partition and boundary analysis on input and output
+     *        Each input: left | right
+     *          Left | Right:
+     *              Partitions:
+     *                  Empty List - Valid
+     *                  Null List - Invalid
+     *                  Integer List w/ non integers - Invalid
+     *                  Integer List w/ negative integers - Invalid
+     *                  Integer List w/ positive integers, 0-9 - Valid
+     *              Boundaries:
+     *                  On-point: 0, 9
+     *                  Off-point: -1, 10
+     *        Combination of input:
+     *          Left & Right:
+     *              Partitions:
+     *                  Left || Right Empty List - Valid
+     *                  Left || Right Null List - Invalid
+     *                  Left || Right Integer List w/ non integers - Invalid
+     *                  Left || Right Integer List w/ negative integers - Invalid
+     *                  Left && Right Integer Lists w/ positive integers, 0-9 - Valid
+     *              Boundaries:
+     *                  On-point: Left && Right Integer List length == 0
+     *                  Off-point None
+     *        Output:
+     *              Partitions:
+     *                  Empty List - Invalid
+     *                  Null List - Invalid
+     *                  Integer List w/ non integers - Invalid
+     *                  Integer List w/ negative integers - Invalid
+     *                  Integer List w/ positive integers, 0-9 - Valid
+     *              Boundaries:
+     *                  On-point: Integer List length == 1
+     *                  Off-point: Integer List length == 0
+     *  Step 3: Derive potential test cases
+     *         1. Left = [], Right = [2], Output = [2]
+     *         2. Left = [1], Right = [], Output = [1]
+     *         3. Left = null, Right = [2], Output = null
+     *         4. Left = [1], Right = null, Output = null
+     *         5. Left = ["A"], Right = [2], Output = IllegalArgumentException
+     *              Unnecessary because add() type checks
+     *         6. Left = [1], Right = ["B"], Output = IllegalArgumentException
+     *              Unnecessary because add() type checks
+     *         7. Left = [-1], Right = [2], Output = IllegalArgumentException
+     *         8. Left = [1], Right = [-2], Output = IllegalArgumentException
+     *         9. Left = [11], Right = [2], Output = IllegalArgumentException
+     *         10. Left = [1], Right = [12], Output = IllegalArgumentException
+     *         11. Left = [1], Right = [2], Output = [3]
+     *         12. Left = [1,2], Right = [3,4], Output = [4,6]
+     *         13. Left = [5,6], Right = [7,8], Output = [1,3,4]
+     *         14. Left = [1], Right = [2,3], Output = [2,4]
+     *         15. Left = [0,1], Right = [2], Output = [3]
+     *         16. Left = [], Right = [], Output = [0]
+     *
+     */
+
+    @Test
+    @Tag("left: Empty")
+    void leftEmpty() {
+        // Test case #1
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(2);
+        right.add(2);
+
+        assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("right: Empty")
+    void rightEmpty() {
+        // Test case #2
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(1);
+        left.add(1);
+
+        assertEquals(expected, util.add(left, right));
+    }
+
+
+
+    @Test
+    @Tag("left: Null")
+    void leftNull() {
+        // Test case #3
+
+        List<Integer> right = new ArrayList<>();
+
+        right.add(2);
+
+        assertNull(util.add(null, right));
+    }
+
+    @Test
+    @Tag("right: Null")
+    void rightNull() {
+        // Test case #4
+
+        List<Integer> left = new ArrayList<>();
+
+        left.add(1);
+
+        assertNull(util.add(left, null));
+    }
+
+    @Test
+    @Tag("left: Negative Integer")
+    void leftNegative() {
+        // Test case #7
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        right.add(2);
+        left.add(-1);
+
+       assertThrows(IllegalArgumentException.class, () -> {
+            util.add(left, right);
+        });
+    }
+
+    @Test
+    @Tag("right: Negative Integer")
+    void rightNegative() {
+        // Test case #8
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        right.add(-2);
+        left.add(1);
+
+       assertThrows(IllegalArgumentException.class, () -> {
+            util.add(left, right);
+        });
+    }
+
+    @Test
+    @Tag("left: Above 9")
+    void leftGreater() {
+        // Test case #9
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        left.add(10);
+        right.add(2);
+
+
+       assertThrows(IllegalArgumentException.class, () -> {
+            util.add(left, right);
+        });
+    }
+
+    @Test
+    @Tag("right: Above 9")
+    void rightGreater() {
+        // Test case #10
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        left.add(1);
+        right.add(12);
+
+       assertThrows(IllegalArgumentException.class, () -> {
+            util.add(left, right);
+        });
+    }
+
+    @Test
+    @Tag("output: Single digit")
+    void outputSingleDigits() {
+        // Test case #11
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(3);
+        left.add(1);
+        right.add(2);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("output: Multiple digits")
+    void outputMultipleDigits() {
+        // Test case #12
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(2);
+        expected.add(3);
+        left.add(1);
+        left.add(1);
+        right.add(1);
+        right.add(2);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("output: Length > Greater Left | Right Length")
+    // Output length is greater than greater of (left | right) length
+    void outputGreaterLength() {
+        // Test case #13
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(1);
+        expected.add(3);
+        expected.add(4);
+        left.add(5);
+        left.add(6);
+        right.add(7);
+        right.add(8);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("output: Length = Greater Length of Left | Right")
+    // Output length is equal length to greater of (left | right) length
+    void outputEqualLength() {
+        // Test case #14
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(2);
+        expected.add(4);
+        left.add(1);
+        right.add(2);
+        right.add(3);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("output: Length = Lesser Length of Left | Right")
+    // Output length is equal length to lesser of (left | right) length
+    void outputLesserLength() {
+        // Test case #15
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(4);
+        left.add(1);
+        right.add(0);
+        right.add(3);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+    @Test
+    @Tag("left: empty, right: empty, output: [0]")
+    @Disabled
+    void emptyLeftRight() {
+        // Test case #15
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(0);
+
+       assertEquals(expected, util.add(left, right));
+    }
+
+}
